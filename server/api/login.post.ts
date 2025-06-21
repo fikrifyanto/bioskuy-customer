ad87e8ae33f01a0c32661b708a2705c90bc8d094
fikrifyanto <fikrifyanto@gmail.com>
2025-06-21 19:48:52 +0700

    Fix login API

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,19 +1,38 @@
+import {z} from 'zod'
+
 const config = useRuntimeConfig()
 
+const bodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
+})
+
 export default defineEventHandler(async (event) => {
-    const {email, password} = await readBody(event)
+    const {email, password} = await readValidatedBody(event, bodySchema.parse)
 
-    console.log('Login:' + email + ' ' + password)
-    const loginResponse = await $fetch<ApiResponse<LoginResponse>>(`${config.public.apiBase}/users/login`, {
-        method: 'POST',
-        body: {email, password},
-        headers: {
-            'Content-Type': 'application/json',
+    try {
+        const loginResponse = await $fetch<ApiResponse<LoginResponse>>(`${config.public.apiBase}/users/login`, {
+            method: 'POST',
+            body: {email, password}
+        })
+
+        const { token, tokenType, customer } = loginResponse.data
+
+        await setUserSession(event, {
+            user: customer,
+            accessToken: `${tokenType} ${token}`,
+        })
+    } catch (error: any) {
+        if (error.response && typeof error.response.status === 'number') {
+            if (error.response.status === 401) {
+                throw createError({statusCode: 401, message: 'Username atau password salah.'})
+            }
+
+            throw createError({
+                statusCode: error.response.status,
+                message: error.response.statusText || 'Terjadi kesalahan!'
+            })
         }
-    })
-
-    await setUserSession(event, {
-        user: loginResponse.data?.customer,
-        accessToken: `${loginResponse.data?.tokenType} ${loginResponse.data?.token}`,
-    })
+        throw createError({statusCode: 500, message: 'Terjadi kesalahan!'})
+    }
 })
