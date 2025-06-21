41512836f9586a7f02f2b2cc0246e9aec15dfb21
fikrifyanto <fikrifyanto@gmail.com>
2025-06-21 19:35:22 +0700

    Fix login API

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -3,35 +3,19 @@ const config = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
     const {email, password} = await readBody(event)
 
-    try {
-        console.log('Login:' + email + ' ' + password)
-        const loginResponse = await $fetch<ApiResponse<LoginResponse>>(`${config.public.apiBase}/users/login`, {
-            method: 'POST',
-            body: {email, password},
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-
-        const { token, tokenType, customer } = loginResponse.data
-
-        await setUserSession(event, {
-            user: customer,
-            accessToken: `${tokenType} ${token}`,
-        })
-    } catch (error: any) {
-        console.log('Login error:' + error)
-        console.log('Login error credential:' + email + ' ' + password)
-        if (error.response && typeof error.response.status === 'number') {
-            if (error.response.status === 401) {
-                throw createError({statusCode: 401, message: 'Username atau password salah.'})
-            }
-
-            throw createError({
-                statusCode: error.response.status,
-                message: error.response.statusText || 'Terjadi kesalahan!'
-            })
+    console.log('Login:' + email + ' ' + password)
+    const loginResponse = await $fetch<ApiResponse<LoginResponse>>(`${config.public.apiBase}/users/login`, {
+        method: 'POST',
+        body: {email, password},
+        headers: {
+            'Content-Type': 'application/json',
         }
-        throw createError({statusCode: 500, message: 'Terjadi kesalahan!'})
-    }
+    })
+
+    const {token, tokenType, customer} = loginResponse.data
+
+    await setUserSession(event, {
+        user: customer,
+        accessToken: `${tokenType} ${token}`,
+    })
 })
